f4402fd78a33436ceee774c95682255f73250c68
Ali Shariatian <shariatian92@gmail.com>
2023-12-07 22:28:21 +0330

    add: main slider container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 // Copy menu for mobile
 const copyMenuMobile = (function () {
-    // copy insede .dpt-cat to .departments
-    let dptCategory = document.querySelector(`.dpt-cat`);
-    let dptPlace = document.querySelector(".departments");
-    dptPlace.innerHTML = dptCategory.innerHTML;
+   // copy insede .dpt-cat to .departments
+   let dptCategory = document.querySelector(`.dpt-cat`);
+   let dptPlace = document.querySelector(".departments");
+   dptPlace.innerHTML = dptCategory.innerHTML;
 
-    // copy inside nav to nav
-    let mainNav = document.querySelector(".header-nav nav");
-    let navPlace = document.querySelector(".off-canvas nav");
-    navPlace.innerHTML = mainNav.innerHTML;
+   // copy inside nav to nav
+   let mainNav = document.querySelector(".header-nav nav");
+   let navPlace = document.querySelector(".off-canvas nav");
+   navPlace.innerHTML = mainNav.innerHTML;
 
-    // copy .header-top .wrapper to .thetop-nav
-    let topNav = document.querySelector(".header-top .wrapper");
-    let topNavPlace = document.querySelector(".off-canvas .thetop-nav");
-    topNavPlace.innerHTML = topNav.innerHTML;
+   // copy .header-top .wrapper to .thetop-nav
+   let topNav = document.querySelector(".header-top .wrapper");
+   let topNavPlace = document.querySelector(".off-canvas .thetop-nav");
+   topNavPlace.innerHTML = topNav.innerHTML;
 })();
 // ---------------------------------------------------------------
 
@@ -22,12 +22,12 @@ const subMenu = document.querySelectorAll(".has-child > a");
 subMenu.forEach((menu) => menu.addEventListener("click", toggleMenu));
 
 function toggleMenu(ev) {
-    ev.preventDefault();
+   ev.preventDefault();
 
-    subMenu.forEach((menu) => (this != menu ? menu.closest(".has-child").classList.remove("expand") : null));
-    if (this.closest(".has-child").classList != "expand") {
-        this.closest(".has-child").classList.toggle("expand");
-    }
+   subMenu.forEach((menu) => (this != menu ? menu.closest(".has-child").classList.remove("expand") : null));
+   if (this.closest(".has-child").classList != "expand") {
+      this.closest(".has-child").classList.toggle("expand");
+   }
 }
 // ---------------------------------------------------------------
 
@@ -37,9 +37,9 @@ const closeMenuBtn = document.querySelector(".t-close");
 const site = document.querySelector(".site");
 
 openMenuBtn.addEventListener("click", () => {
-    site.classList.add("show-menu");
+   site.classList.add("show-menu");
 });
 
 closeMenuBtn.addEventListener("click", () => {
-    site.classList.remove("show-menu");
+   site.classList.remove("show-menu");
 });
